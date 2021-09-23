0c903541a7d6de72c7c37dba513e0f9cd57f8012
RakeshThakur26 <rakesh.thakur.mail99@gmail.com>
2021-09-23 12:16:02 +0530

    Hands On Collection

---
+++ HandsOnCollection/HandsOnCollection/Consumer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnCollection
+{
+    class Consumer
+    {
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+
+        }
+
+        private string name;
+        public string Name {
+            get { return name; }
+            set { name = value; } 
+        }
+        private int totalUnits;
+        public int TotalUnits {
+            get { return totalUnits; }
+            set {totalUnits = value; } 
+        }
+
+        private int totalAmount;
+        public int TotalAmount {
+            get {return totalAmount; }
+            set {totalAmount = value; } 
+        }
+
+        private int costPerUnit;
+        public int CostPerUnit {
+            get { return costPerUnit; }
+            set { costPerUnit = value; }
+        }
+
+        private int billNumber;
+        public int BillNumber {
+            get { return billNumber; }
+            set { billNumber = value; }
+        }
+
+        private DateTime date;
+        public DateTime Date {
+            get {return date; }
+            set {date = value; } 
+        }
+       
+        
+    }
+
+}
---
+++ HandsOnCollection/HandsOnCollection/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of Consumers ");
+            int n = Convert.ToInt32(Console.ReadLine());
+           
+            Consumer[] consumer = new Consumer[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                consumer[i] = new Consumer();
+                Console.WriteLine("Enter Consumer " + i + " Details..");
+                Console.WriteLine("***************");
+                Console.WriteLine("Enter consumer number : ");
+                consumer[i].Number = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter consumer Name : ");
+                consumer[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Enter Total Units : ");
+                consumer[i].TotalUnits = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Cost per Unit : ");
+                consumer[i].CostPerUnit = Convert.ToInt32(Console.ReadLine());
+
+                consumer[i].TotalAmount = consumer[i].TotalUnits * consumer[i].CostPerUnit;
+                Console.WriteLine();
+                Random r = new Random();
+
+                consumer[i].BillNumber = r.Next(100);
+                consumer[i].Date = DateTime.Now;
+            }
+
+            List<Consumer> obj = new List<Consumer>();
+            for (int i = 0; i < n; i++)
+            {
+                obj.Add(consumer[i]);
+            }
+
+            foreach (Consumer item in obj)
+            {
+                Console.WriteLine("Consumer Bill : ");
+                Console.WriteLine("Bill number : " + item.BillNumber + ", Consumer number : " + item.Number + ", Total Amount : " + item.TotalAmount + ", Due date : " + item.Date);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
